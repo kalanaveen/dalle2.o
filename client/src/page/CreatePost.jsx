6ab65b619da0ebd8ee329ff20430100c4c8b6abd
naveen kala <kalanaveen55@gmail.com>
2023-05-04 15:06:41 +0530

    utils add

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { FormField,Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo:'',
+  });
+  
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>
+    <d>
       createpost
-    </div>
+    </d>
   )
 }
 
